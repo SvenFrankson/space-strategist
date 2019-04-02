2bc4f7e85e2c07388385bb947cdb29fd1a43e3cf
SvenFrankson <sven.frankson@gmail.com>
2019-04-02 02:35:41 +0200

    Add Outline post-process.

--- scripts/app.js
+++ scripts/app.js
@@ -17,6 +17,53 @@ class Main {
         var camera = new BABYLON.ArcRotateCamera("camera1", 0, 0, 1, BABYLON.Vector3.Zero(), Main.Scene);
         camera.setPosition(new BABYLON.Vector3(0, 5, -10));
         camera.attachControl(Main.Canvas, true);
+        BABYLON.Effect.ShadersStore["EdgeFragmentShader"] = `
+			#ifdef GL_ES
+			precision highp float;
+			#endif
+			varying vec2 vUV;
+			uniform sampler2D textureSampler;
+			uniform sampler2D depthSampler;
+			uniform float 		width;
+			uniform float 		height;
+			void make_kernel(inout vec4 n[9], sampler2D tex, vec2 coord)
+			{
+				float w = 1.0 / width;
+				float h = 1.0 / height;
+				n[0] = texture2D(tex, coord + vec2( -w, -h));
+				n[1] = texture2D(tex, coord + vec2(0.0, -h));
+				n[2] = texture2D(tex, coord + vec2(  w, -h));
+				n[3] = texture2D(tex, coord + vec2( -w, 0.0));
+				n[4] = texture2D(tex, coord);
+				n[5] = texture2D(tex, coord + vec2(  w, 0.0));
+				n[6] = texture2D(tex, coord + vec2( -w, h));
+				n[7] = texture2D(tex, coord + vec2(0.0, h));
+				n[8] = texture2D(tex, coord + vec2(  w, h));
+			}
+			void main(void) 
+			{
+				vec4 d = texture2D(depthSampler, vUV);
+				float depth = d.r * (2000.0 - 0.5) + 0.5;
+				vec4 n[9];
+				make_kernel( n, textureSampler, vUV );
+				vec4 sobel_edge_h = n[2] + (2.0*n[5]) + n[8] - (n[0] + (2.0*n[3]) + n[6]);
+				vec4 sobel_edge_v = n[0] + (2.0*n[1]) + n[2] - (n[6] + (2.0*n[7]) + n[8]);
+				vec4 sobel = sqrt((sobel_edge_h * sobel_edge_h) + (sobel_edge_v * sobel_edge_v));
+				float threshold = 0.4 + max((depth - 10.) / 30., 0.);
+				if (max(sobel.r, max(sobel.g, sobel.b)) < threshold) {
+					gl_FragColor = n[4];
+				} else {
+					gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+				}
+			}
+        `;
+        let depthMap = Main.Scene.enableDepthRenderer(camera).getDepthMap();
+        var postProcess = new BABYLON.PostProcess("Edge", "Edge", ["width", "height"], ["depthSampler"], 1, camera);
+        postProcess.onApply = (effect) => {
+            effect.setTexture("depthSampler", depthMap);
+            effect.setFloat("width", Main.Engine.getRenderWidth());
+            effect.setFloat("height", Main.Engine.getRenderHeight());
+        };
         new VertexDataLoader(Main.Scene);
         new NavGraphManager();
         let start = new BABYLON.Vector2(0, -10);
